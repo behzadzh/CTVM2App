347bbaef873c70a1c5fdcb103ebce2b204186c57
Behzad.Naderloo <b.naderloo@gmail.com>
2018-07-16 15:03:00 +0430

    initial commit - SignalR is set up , but the app is still pretty lame ... :(

---
+++ CTVM2App/Scripts/SignalR.js
@@ -0,0 +1,14 @@
+﻿//immediate invoked function expression (IIFE)
+(function() {
+    $.connection.hub.start()
+        .done(function () {
+            console.log("IT WORK!");
+            $.connection.myHub.server.announce("Connected!");
+        })
+        .fail(function () { alert("ERROR!!!!"); });
+
+    $.connection.myHub.client.announce = function (message) {
+
+        $("#welcome-messages").append(message + "<br />");
+    }
+})()
